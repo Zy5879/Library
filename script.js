db94cc383de81ef09a8e5efa710628482afbfcbe
Zy5879 <ZaireM21@gmail.com>
2022-09-14 14:33:18 -0400

    added a read button that can toggle

--- script.js
+++ script.js
@@ -22,6 +22,7 @@ closeModalButtons.forEach(button => {
     button.addEventListener('click', () => {
         const modal = button.closest('.modal')
         closeModal(modal)
+
     })
 })
 
@@ -29,7 +30,6 @@ function openModal(modal) {
     if (modal == null) return
     modal.classList.add('active')
     overlay.classList.add('active')
-
 }
 
 function closeModal(modal) {
@@ -38,15 +38,15 @@ function closeModal(modal) {
     overlay.classList.remove('active')
 }
 
-function Book(book,author,page, isRead) {
+function Book(book,author,page,read) {
     this.book = book;
     this.author = author;
     this.page = page;
-    this.isRead = isRead
+    this.read = read;
 }
 
-function addBookToLibrary(book,author,page, isRead) {
-    let news = new Book(book, author, page, isRead)
+function addBookToLibrary(book,author,page,read) {
+    let news = new Book(book, author, page,read)
     library.push(news)
     // displayBooks()
 }
@@ -58,18 +58,22 @@ function displayBooks() {
     const writer = document.getElementById('inputValueAuthor').value;
     const pg = document.getElementById('inputPages').value;
     
-    
-    const readBtn = document.getElementById('readStatus').value;
-
-
-    // const readBtn = document.getElementById('read-btn').value;
+    const readBtn = document.getElementById('read')
 
     const para = document.createElement('p');
     const para1 = document.createElement('p');
     const para2 = document.createElement('p');
 
     const btn2  = document.createElement('button');
-    console.log(btn2)
+    btn2.classList.add('btn2');
+    if(readBtn.checked === true) {
+        btn2.innerHTML = 'Read';
+        btn2.value = 'Read'
+    } else {
+        btn2.innerHTML = 'Did Not Read';
+        btn2.value = 'Did Not Read';
+    }
+
     btn2.addEventListener('click', change)
     function change() {
         if(btn2.value === 'Read') {
@@ -81,7 +85,6 @@ function displayBooks() {
         }
     }
 
-    // let index = 0; 
     const card = document.createElement('div');
     card.classList.add('card');
     main.appendChild(card)
@@ -90,50 +93,35 @@ function displayBooks() {
     removeBookButton.classList.add('remove-book-button');
     removeBookButton.textContent = 'x';
 
-    // removeBookButton.dataset.linkedArray = index;
-    // index++;
     card.appendChild(removeBookButton)
 
     removeBookButton.addEventListener('click', function(e) {
-        this.parentNode.remove()
+        this.parentNode.remove();
         let target = e.target;
         let itemIndex = 0;
-        for(const item of items) {
+        for(const item of library) {
             if(item.index === target.parentNode.index) {
-                items.splice(item.index, 1);
-                items.Children[item.index]. remove();
+                library.splice(item.index, 1);
                 break;
             }
             itemIndex++;
         }
     });
     
-    // function removeBookFromLibrary() {
-    //     let retriveBookToRemove = removeBookButton.dataset.linkedArray;
-    //     library.splice(retriveBookToRemove, 1);
-    //     card.remove()
-    // }
 
 
-    // para.textContent = (`${title} ${writer} ${pg}`);
     para.textContent = (`${title}`);
     para1.textContent = (`${writer}`);
     para2.textContent = (`${pg}`);
-    btn2.textContent = (`${readBtn}`)
     card.append(para, para1, para2, btn2);
-    addBookToLibrary(title, writer, pg, btn2);
+    addBookToLibrary(title, writer, pg, readBtn.checked);
     closeModal(modal)
 
 }
 
 
 
-// addBookToLibrary('Harry Potter', 'JK ROWLING', '256');
-// addBookToLibrary('Harry Potter', 'JK ROWLING', '256');
-
-
 const btn = document.getElementById('btn').addEventListener('click', displayBooks);
-
 
 
 
