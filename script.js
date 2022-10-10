0ab10721095bd802c2d504a6df278c2b25bdbea2
Zy5879 <ZaireM21@gmail.com>
2022-10-09 21:53:11 -0400

    changed some of my functions to classes

--- script.js
+++ script.js
@@ -40,114 +40,196 @@ function closeModal(modal) {
     overlay.classList.remove('active')
 }
 
+// const main = document.querySelector('.main')
+// const newBook = new Book(main)
 
-
-// Object Constructor
-
-function Book(book,author,page,read) {
-    this.book = book;
-    this.author = author;
-    this.page = page;
-    this.read = read;
+class Book {
+    constructor(title,author,page,read) {
+        this.title = title;
+        this.author = author;
+        this.page = page;
+        this.read = read;
+    }
 }
 
-// pushes info from Book Constructor to library array
+class Storage {
+    static displayBooks() {
+        const StoredBooks =  [
+            {
+                title: 'Bk',
+                author: 'JK',
+                page: 245,
+                read: 'Read'
+            },
+            {
+               title: 'Bk',
+               author: 'JK',
+               page: 245,
+               read: 'Did Not Read'
+            }
+                
+        ];
 
-function addBookToLibrary(book,author,page,read) {
-    let news = new Book(book, author, page,read)
-    library.push(news)
+        const books = StoredBooks;
+        books.forEach((book) => Storage.addBookToLibrary(book));
+    }
+
+    static addBookToLibrary(book) {
+        const main = document.querySelector('.main');
+        const card = document.createElement('div');
+        card.classList.add('card')
+        // const btn2 = document.createElement('button')
+        // btn2.classList.add('btn2')
+
+        card.innerHTML = `
+            <p>${book.title}</p>
+            <p>${book.author}</p>
+            <p>${book.page}</p>
+            <button>${book.read}</button>
+        `;
+
+        // card.append(btn2)
+
+
+        main.appendChild(card)
+
+    }
+
 }
 
-// Display info that's pushed into array on page
+document.addEventListener('DOMContentLoaded', Storage.displayBooks)
 
-function displayBooks() {
-    const main = document.querySelector('.main');
-    
+document.getElementById('btn').addEventListener('click', (e) => {
+    console.log(e);
+    e.preventDefault();
+
     const title = document.getElementById('inputValueBook').value;
     const writer = document.getElementById('inputValueAuthor').value;
     const pg = document.getElementById('inputPages').value;
     const readBtn = document.getElementById('read')
 
-    const para = document.createElement('p');
-    const para1 = document.createElement('p');
-    const para2 = document.createElement('p');
+    const book = new Book(title,writer,pg,readBtn.checked);
 
-    // if checkbox is checked it returns Read in HTML BUTTON once submit is clicked
-
-    const btn2  = document.createElement('button');
-    btn2.classList.add('btn2');
     if(readBtn.checked === true) {
-        btn2.innerHTML = 'Read';
-        btn2.value = 'Read'
-        btn2.style.backgroundColor = '#39FF14'
+        book.read = 'Read'
     } else {
-        btn2.innerHTML = 'Did Not Read';
-        btn2.value = 'Did Not Read';
-        btn2.style.backgroundColor = '#FF3131'
+        book.read = 'Did Not Read'
     }
 
-    // After submit allows toggle 
-
-    btn2.addEventListener('click', change)
-    function change() {
-        if(btn2.value === 'Read') {
-            btn2.value = 'Did Not Read';
-            btn2.innerHTML = 'Did Not Read';
-            btn2.style.backgroundColor = '#FF3131'
-        } else {
-            btn2.value = 'Read';
-            btn2.innerHTML = 'Read';
-            btn2.style.backgroundColor = '#39FF14'
-        }
-    }
+    Storage.addBookToLibrary(book)
+})
 
 
-    const card = document.createElement('div');
-    card.classList.add('card');
-    main.appendChild(card)
 
-    const removeBookButton = document.createElement('button')
-    removeBookButton.classList.add('remove-book-button');
-    removeBookButton.textContent = 'x';
+// const btn = document.getElementById('btn').addEventListener('click', Storage.addBookToLibrary);
 
-    card.appendChild(removeBookButton)
 
-    // Allows to delete a specific card in your library
+// Object Constructor
 
-    removeBookButton.addEventListener('click', function(e) {
-        this.parentNode.remove();
-        let target = e.target;
-        let itemIndex = 0;
-        for(const item of library) {
-            if(item.index === target.parentNode.index) {
-                library.splice(item.index, 1);
-                break;
-            }
-            itemIndex++;
-        }
-    });
+// function Book(book,author,page,read) {
+//     this.book = book;
+//     this.author = author;
+//     this.page = page;
+//     this.read = read;
+// }
+
+// pushes info from Book Constructor to library array
+
+// function addBookToLibrary(book,author,page,read) {
+//     let news = new Book(book, author, page,read)
+//     library.push(news)
+// }
+
+// Display info that's pushed into array on page
+
+// function displayBooks() {
+    // const main = document.querySelector('.main');
+    
+    // const title = document.getElementById('inputValueBook').value;
+    // const writer = document.getElementById('inputValueAuthor').value;
+    // const pg = document.getElementById('inputPages').value;
+    // const readBtn = document.getElementById('read')
+
+    // const para = document.createElement('p');
+    // const para1 = document.createElement('p');
+    // const para2 = document.createElement('p');
+
+//     // if checkbox is checked it returns Read in HTML BUTTON once submit is clicked
+
+    // const btn2  = document.createElement('button');
+    // btn2.classList.add('btn2');
+    // if(readBtn.checked === true) {
+    //     btn2.innerHTML = 'Read';
+    //     btn2.value = 'Read'
+    //     btn2.style.backgroundColor = '#39FF14'
+    // } else {
+    //     btn2.innerHTML = 'Did Not Read';
+    //     btn2.value = 'Did Not Read';
+    //     btn2.style.backgroundColor = '#FF3131'
+    // }
+
+//     // After submit allows toggle 
+
+    // btn2.addEventListener('click', change)
+    // function change() {
+    //     if(btn2.value === 'Read') {
+    //         btn2.value = 'Did Not Read';
+    //         btn2.innerHTML = 'Did Not Read';
+    //         btn2.style.backgroundColor = '#FF3131'
+    //     } else {
+    //         btn2.value = 'Read';
+    //         btn2.innerHTML = 'Read';
+    //         btn2.style.backgroundColor = '#39FF14'
+    //     }
+    // }
+
+
+    // const card = document.createElement('div');
+    // card.classList.add('card');
+    // main.appendChild(card)
+
+    // const removeBookButton = document.createElement('button')
+    // removeBookButton.classList.add('remove-book-button');
+    // removeBookButton.textContent = 'x';
+
+    // card.appendChild(removeBookButton)
+
+//     // Allows to delete a specific card in your library
+
+    // removeBookButton.addEventListener('click', function(e) {
+    //     this.parentNode.remove();
+    //     let target = e.target;
+    //     let itemIndex = 0;
+    //     for(const item of library) {
+    //         if(item.index === target.parentNode.index) {
+    //             library.splice(item.index, 1);
+    //             break;
+    //         }
+    //         itemIndex++;
+    //     }
+    // });
     
 
-    //value of your input gets pushed onto the page and into the array.
+//     // value of your input gets pushed onto the page and into the array.
 
-    para.textContent = (`${title}`);
-    para1.textContent = (`${writer}`);
-    para2.textContent = (`${pg}`);
-    card.append(para, para1, para2, btn2);
-    addBookToLibrary(title, writer, pg, readBtn.checked);
-    closeModal(modal)
-    myForm.reset()
-}
-
-
+    // para.textContent = (`${title}`);
+    // para1.textContent = (`${writer}`);
+    // para2.textContent = (`${pg}`);
+    // card.append(para, para1, para2, btn2);
+    // addBookToLibrary(title, writer, pg, readBtn.checked);
+    // closeModal(modal)
+    // myForm.reset()
+// }
 
 
-const btn = document.getElementById('btn').addEventListener('click', displayBooks);
 
-//Prevents form from reloading once submit is clicked
 
-document.addEventListener('DOMContentLoaded', (event) => {
-    document.getElementById('myForm').addEventListener('submit', function(e) {
-        e.preventDefault()
-    })
-})
+// const btn = document.getElementById('btn').addEventListener('click', displayBooks);
+
+// Prevents form from reloading once submit is clicked
+
+// document.addEventListener('DOMContentLoaded', (event) => {
+//     document.getElementById('myForm').addEventListener('submit', function(e) {
+//         e.preventDefault()
+//     })
+// })
